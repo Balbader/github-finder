7678cf9cd864cbb028d2ffe70bb4e16755e43bf0
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-15 21:28:03 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
