e04bd1145a39afa67504de42f32e84669b95c298
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-18 16:47:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -22,12 +22,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // Clear users form state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
