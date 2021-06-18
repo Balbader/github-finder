da17b38b8df933324700655c3cab47f5e3979d5f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-18 18:06:18 +0200

    update

--- src/App.js
+++ src/App.js
@@ -30,8 +30,15 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users
+            loading={this.state.loading}
+            users={this.state.users}
+          />
         </div>
       </div>
     );
