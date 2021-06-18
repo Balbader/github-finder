58c4a57b1b53defd683c4a949ab4f9aa3c162fb9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-18 16:26:21 +0200

    update

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,34 @@
 import React, { PureComponent } from 'react'
 
 export default class Search extends PureComponent {
+	state = {
+		text: ''
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({ text: '' });
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
 	render() {
 		return (
 			<div>
-				<form className='form'>
-					<input type='text' name='text' placeholder='Search User' />
-					<input type='submit' value='Search' className='btn-dark btn-block' />
+				<form onSubmit={this.onSubmit} className='form'>
+					<input
+						type='text'
+						name='text'
+						placeholder='Search User'
+						value={this.state.text}
+						onChange={this.onChange}
+					/>
+					<input
+						type='submit'
+						value='Search'
+						className='btn-dark btn-block'
+					/>
 				</form>
 			</div>
 		)
