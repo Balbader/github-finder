7215966483808325cc1ba019012b7cba69d34d99
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-18 16:26:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -13,14 +13,22 @@ class App extends Component {
 
   async componentDidMount() {
 
-    this.setState({ loading: true });
+    // this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?client_id=$
+    // const res = await axios.get(`https://api.github.com/users?client_id=$
+    // {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    // {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    // this.setState({ users: res.data.items, loading: false });
+  }
+
+  // Search Github Users
+  searchUsers = async text => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ users: res.data, loading: false });
-
+    this.setState({ users: res.data.items, loading: false });
   }
 
   render() {
@@ -28,7 +36,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
