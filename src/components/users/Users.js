8a151d15c38807299991bf5a938028fc083a1e13
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-18 20:22:32 +0200

    udpate

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -5,14 +5,15 @@ import PropTypes from 'prop-types';
 const Users = ({ users, loading }) => {
 	if (loading) {
 		return <img src={Spinner} alt='Loading...' />
+	} else {
+		return (
+			<div style={userStyle}>
+				{users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
 	}
-	return (
-		<div style={userStyle}>
-			{users.map(user => (
-				<UserItem key={user.id} user={user} />
-			))}
-		</div>
-	);
 }
 
 Users.propTypes = {
