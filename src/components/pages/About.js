ca5b047515c16d609d9f5af37a1e317c0a5cc415
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 12:40:20 +0200

    update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
 	return (
@@ -6,9 +7,15 @@ const About = () => {
 			<h1>About This App</h1>
 			<p>App to search Github users</p>
 			<p>Version: 1.0.0</p>
-			<button href='#' className='btn btn-dark'>Back to Main Page</button>
 		</Fragment>
 	)
 }
 
 export default About
+
+/*
+
+			<Link to='/'>
+				<button className='btn btn-dark'>Back to Main Page</button>
+			</Link>
+ */
