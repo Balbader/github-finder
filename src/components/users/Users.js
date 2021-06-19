0718487f6e96b5a644440726e1611f0c8611f3ea
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 10:49:11 +0200

    update

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,17 @@
 import UserItem from './UserItem';
-import Spinner from '../layout/spinner.gif';
+import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
-	if (loading) {
-		return <img src={Spinner} alt='Loading...' style={{ width: '200px', margin: 'auto', display: 'block' }} />
-	} else {
-		return (
-			<div style={userStyle}>
-				{users.map(user => (
-					<UserItem key={user.id} user={user} />
-				))}
-			</div>
-		);
-	}
+	if (loading) return <Spinner />;
+
+	return (
+		<div style={userStyle}>
+			{users.map(user => (
+				<UserItem key={user.id} user={user} />
+			))}
+		</div>
+	);
 }
 
 Users.propTypes = {
