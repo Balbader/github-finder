116368040f6f13024efa8c2fbae7aac9cd436919
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 12:28:49 +0200

    updtae

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+	return (
+		<Fragment>
+			<h1>About This App</h1>
+			<p>App to search Github users</p>
+			<p>Version: 1.0.0</p>
+			<button href='#' className='btn btn-dark'>Back to Main Page</button>
+		</Fragment>
+	)
+}
+
+export default About
