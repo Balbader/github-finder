91540b82902ae5b45bf0fe3e7b29c615174aa690
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 10:37:22 +0200

    upate

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,7 +1,11 @@
 import spinner from './spinner.gif';
 
 const Spinner = () => {
-			<img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
+	<img
+		src={spinner}
+		alt="Loading..."
+		style={{ width: '200px', margin: 'auto', display: 'block' }}
+	/>
 }
 
 export default Spinner
