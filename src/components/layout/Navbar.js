7b0350280193d399c99045bc7d2d9c85a0d60146
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 12:28:40 +0200

    update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,6 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
 	return (
