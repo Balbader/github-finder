bb5d671b46038b9b8bf8e7b69549af507b4696d7
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 11:26:00 +0200

    update

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,10 +1,12 @@
+import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-const Spinner = () => <img
+const Spinner = () => <Fragment>
+	<img
 		src={spinner}
 		alt="Loading..."
 		style={{ width: '200px', margin: 'auto', display: 'block' }}
 	/>
-
+</Fragment>
 
 export default Spinner
