37da8806845aaad9b3fcfdff7f3c2f8141cacc24
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-19 11:44:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert'
 import axios from 'axios'
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // Search Github Users
@@ -25,6 +27,11 @@ class App extends Component {
   // Clear users form state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  // Alert users when search field is empty
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg, type} });
+  }
+
   render() {
     const { users, loading } = this.state;
 
@@ -32,10 +39,12 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
+        <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users
             loading={loading}
