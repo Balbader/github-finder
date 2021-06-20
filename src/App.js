e87fcedaf0a7b7d119d4d535463371e921f42203
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-20 19:03:00 +0200

    update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    repos: [],
     user: {},
     loading: false,
     alert: null
@@ -39,6 +40,17 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   }
 
+  // Get user Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   // Clear users form state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -51,7 +63,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -80,7 +92,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
