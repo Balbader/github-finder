d385f7a168231e389c451168c61b4539045760ae
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-20 13:14:45 +0200

    update

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -60,10 +60,17 @@ export class User extends Component {
 						<p>{location}</p>
 					</div>
 					<div>
-						{bio && <Fragment>
+						{bio && (<Fragment>
 							<h2><strong>Bio</strong></h2>
 							<p>{bio}</p>
-						</Fragment>}
+						</Fragment>
+						)}
+						<a
+							href={html_url}
+							className='btn btn-dark my-1'
+						>
+							Visit Github Profile
+						</a>
 					</div>
 				</div>
 			</Fragment>
