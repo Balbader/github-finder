50abcce78afdc1daa2afe764b903f352851b07af
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-20 11:37:32 +0200

    update

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,6 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
@@ -12,7 +14,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 			/>
 			<h3>{login}</h3>
 			<div>
-				<a href={html_url} className="btn btn-dark btn-sm">More</a>
+				<Link to={`/user/${login}`} className="btn btn-dark btn-sm">
+					More
+				</Link>
 			</div>
 		</div>
 	);
