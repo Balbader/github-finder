92c90702550d9a38e9acc5de19898fe7c615dd72
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-20 11:55:45 +0200

    update

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class User extends Component {
+
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.login);
+	}
+
 	render() {
+
+		const {
+			name,
+			avatar_url,
+			location,
+			bio,
+			blot,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+
+		const { loading } = this.props;
+
 		return (
 			<div>
-				User
+				<h1>User</h1>
 			</div>
 		)
 	}
