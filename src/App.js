f30153c836a42ce796b634d529455856e5caa819
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-21 16:33:19 +0200

    refactoring to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -9,54 +9,58 @@ import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    repos: [],
-    user: {},
-    loading: false,
-    alert: null
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
   // Search Github Users
   searchUsers = async text => {
-    this.setState({ loading: true });
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ users: res.data.items, loading: false });
+    setUsers(res.data.items);
+    setLoading(false);
   }
 
   // Get single Github user
   getUser = async username => {
-    this.setState({ loading: true });
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ user: res.data, loading: false });
+    setUser(res.data);
+    setLoading(false);
   }
 
   // Get user Repos
   getUserRepos = async username => {
-    this.setState({ loading: true });
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ repos: res.data, loading: false });
+    setRepos(res.data);
+    setLoading(false);
   }
 
   // Clear users form state
-  clearUsers = () => this.setState({ users: [], loading: false });
+  clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
+  };
 
   // Alert users when search field is empty
   setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
+    setAlert({ msg, type });
 
     // setTimeout to remove the alert from the screen after 5sec
     setTimeout(() => this.setState({ alert: null }), 5000);
