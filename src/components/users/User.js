a4fc97e9f3857bfe198c8bbfbc6246ad88b91008
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-21 15:39:52 +0200

    update

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,23 +1,15 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-export class User extends Component {
+const User = () => {
 
 	componentDidMount() {
 		this.props.getUser(this.props.match.params.login);
 		this.props.getUserRepos(this.props.match.params.login);
-	}
-
-	static propTypes = {
-		loading: PropTypes.bool,
-		user: PropTypes.object.isRequired,
-		repos: PropTypes.array.isRequired,
-		getUser: PropTypes.func.isRequired,
-		getUserRepos: PropTypes.func.isRequired
 	}
 
 	render() {
@@ -120,5 +112,13 @@ export class User extends Component {
 		)
 	}
 }
+
+	User.propTypes = {
+		loading: PropTypes.bool,
+		user: PropTypes.object.isRequired,
+		repos: PropTypes.array.isRequired,
+		getUser: PropTypes.func.isRequired,
+		getUserRepos: PropTypes.func.isRequired
+	}
 
 export default User
