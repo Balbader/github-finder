f4147fa51980b1b6a92c93a589e400f8385ebe6f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-22 15:10:45 +0200

    update

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,7 +20,17 @@ const GithubState = props => {
 
 	const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-	// Search Users
+	// Search Github Users
+	const searchUsers = async text => {
+		setLoading(true);
+	
+		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
+		{process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+		{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+	
+		setUsers(res.data.items);
+		setLoading(false);
+	  }
 
 	// Get Users
 
