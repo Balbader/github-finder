5c1846be2e0c3eb9b8b588f2cc2f35aae0530c6d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-22 18:18:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -6,49 +6,42 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import './App.css';
-
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  // Alert users when search field is empty
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    // setTimeout to remove the alert from the screen after 5sec
-    setTimeout(() => setAlert(null), 5000);
-  }
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search
-                    setAlert={showAlert}
-                  />
-                  <Users
-                    loading={loading}
-                  />
-                </Fragment>
-              )} />
-              <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                component={User}
-              />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' render={props => (
+                  <Fragment>
+                    <Search />
+                    <Users />
+                  </Fragment>
+                )} />
+                <Route
+                  exact
+                  path='/about'
+                  component={About}
+                />
+                <Route
+                  exact
+                  path='/user/:login'
+                  component={User}
+                />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 }
