daf5d9e4e66eb7c3de3af5b5a4a35ab79faf7650
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-22 15:10:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -17,18 +17,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
   // Get single Github user
   const getUser = async username => {
     setLoading(true);
